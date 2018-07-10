97a3814ae742f611954bc33e992016aaf5f4a5b4
Shivanshu <40226602+shi1711garg@users.noreply.github.com>
2018-07-10 15:39:21 +0530

    Author:Shivanshu Garg Files added: StateDetails.java,CandidatesTrainedInLast6Months.java Description: Dashboard Statistics story | NADUDF 6

---
+++ NSKFDC App/nskfdc-app-common/src/main/java/com/nskfdc/scgj/dto/CandidatesTrainedInLast6Months.java
@@ -0,0 +1,52 @@
+package com.nskfdc.scgj.dto;
+import com.nskfdc.scgj.common.BaseDto;
+
+
+public class CandidatesTrainedInLast6Months extends BaseDto{
+	
+	/**
+	 * @author Shivanshu Garg
+	 * @description Initialising data members to get  total number of candidates trained in last 6 months
+	 */
+	
+	private String month;
+	private int count;
+	
+	public String getmonth() {
+		return month;
+	}
+	public void setmonth(String month) {
+		this.month = month;
+	}
+	
+	public int getcount() {
+		return count;
+	}
+	public void setcount(int count) {
+		this.count = count;
+	}
+	
+	/** @author Shivanshu Garg
+	 * @description Parameterised constructor to map result set of candidates trained in last 6 months
+	 *
+	 **/
+	
+
+	public CandidatesTrainedInLast6Months() {
+		super();
+	
+	}
+	
+	
+
+	public CandidatesTrainedInLast6Months(String month, int count) {
+		super();
+		
+		this.month = month;
+		this.count = count;
+	}
+
+	
+	
+}
+
