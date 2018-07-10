6f6462c257d4661256da474388a9567d66bd4fd9
sagun01 <40224371+sagun01@users.noreply.github.com>
2018-07-10 15:02:31 +0530

    Author: Pranjal Kumar Mani File: ImportController.java Description: Controller file for DataImport

--- NSKFDC App/nskfdc-app-common/src/main/java/com/nskfdc/scgj/controller/ImportController.java
+++ NSKFDC App/nskfdc-app-common/src/main/java/com/nskfdc/scgj/controller/ImportController.java
@@ -3,6 +3,7 @@ package com.nskfdc.scgj.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -67,5 +68,33 @@ public class ImportController {
 				//return the default value, it can be null
 			}
 		}
+
+	 @RequestMapping("/getDataImport")
+	 public int generateBatchController(){
+		 String trainingPartnerEmail = "snb@gmail.com";
+		 LOGGER.debug("Request received from frontend to create batch for email id : " + trainingPartnerEmail);
+		
+		 LOGGER.debug("In Import Controller to create batch for rmail id: " + trainingPartnerEmail);
+		 try{
+			
+			 return importHistoryService.getGenerateBatchService(trainingPartnerEmail);
+				
+		 }
+		
+		 catch(DataAccessException d) {
+			
+			 LOGGER.error("DataAccessException in controller to create Batch" + d);
+			
+			 return -1;
+		 }
+		
+		 catch(Exception e) {
+		
+			 LOGGER.error("An error occurred while creating Batch" + e);
+				
+			 return -1;
+		 }
+
+	 }
 
 }
