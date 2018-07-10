648910db8291a7c10e2f984f8980b823eef63354
Sarthak <sarthakbhutani.work@gmail.com>
2018-07-10 13:24:41 +0530

    Author        : Sarthak (Reviewed by Prateek| Before Code Review) File Modified : dashboardController.js Description   : Made the Angular Fronntend (including Service & Controller) for the dashboard page for dashboard story

--- NSKFDC App/nskfdc-app-server/src/main/resources/static/js/controllers/dashboardController.js
+++ NSKFDC App/nskfdc-app-server/src/main/resources/static/js/controllers/dashboardController.js
@@ -2,6 +2,28 @@ var scgj = angular.module("app");
 
 scgj.controller("dashboardController" , function($scope, $http){
 	
+	$http.get('/getNumberOfOngoingTrainings')
+    .then(function (response) {
+    	$scope.ongoingTrainings = response.data;
 
+    });
+
+	$http.get('/getNumberOfCandidatesTrained')
+    .then(function (response) {
+    	$scope.candidatesTrained = 2500;//response.data;
+
+    });
+	
+	$http.get('/getNumberOfTrainingPartners')
+    .then(function (response) {
+    	 $scope.trainingPartners = response.data;
+
+    });
+	
+	$http.get('/getNumberOfUpcomingAssessments')
+    .then(function (response) {
+    	$scope.upcomingAssesments = response.data;
+
+    });
 
 });
