9ea441c2e45d03100c015e90a877e4b1b09cbe62
sagun01 <40224371+sagun01@users.noreply.github.com>
2018-07-10 15:03:10 +0530

    Author: Pranjal Kumar Mani File: ImportDao.java Description: Dao file for DataImport

--- NSKFDC App/nskfdc-app-common/src/main/java/com/nskfdc/scgj/dao/ImportDao.java
+++ NSKFDC App/nskfdc-app-common/src/main/java/com/nskfdc/scgj/dao/ImportDao.java
@@ -1,8 +1,12 @@
 package com.nskfdc.scgj.dao;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Repository;
 
 import com.nskfdc.scgj.common.AbstractTransactionalDao;
@@ -64,6 +68,47 @@ public class ImportDao extends AbstractTransactionalDao{
 				
 			}
 
+	
+	public Integer generateBatchDao(String trainingPartnerEmail){
+					
+			LOGGER.debug("Request received from Service");
+					
+			LOGGER.debug("In  Import Dao, to get create batch for email id:" + trainingPartnerEmail);
+					
+			Map<String, Object> parameters = new HashMap<>();
+					
+			parameters.put("trainingPartnerEmail",trainingPartnerEmail);
+					
+			if(parameters.isEmpty())
+			{
+				LOGGER.error("Null Parameter");
+			}
+			
+			try{
+			        
+				Integer result = getJdbcTemplate().update(dataImportConfig.getImportBatchDetails(),parameters);
+						
+				LOGGER.debug("The result of the query is : " + result);
+						
+				return result;
+			}
+					
+			catch(DataAccessException d) {
+						
+				LOGGER.error("DataAccessException in Dao" + d);
+						
+				return -1;
+			}
+					
+			catch(Exception e) {
+						
+				LOGGER.error("In Catch Block");
+						
+				LOGGER.error("An error occured while generating the batch" + e);
+						
+				return -1;
+			}
+	}
 			
 			//write Rowmapper class here
 	
