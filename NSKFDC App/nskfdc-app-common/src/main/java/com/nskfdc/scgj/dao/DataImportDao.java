edbb9f298351043600abe867914b7ec9cf5bb2f7
Pranjal Kumar Mani <38162040+pranjalmani123@users.noreply.github.com>
2018-07-11 11:26:14 +0530

    Author :  Pranjal Kumar Mani File Modified  :  DataImportDao.java Description  :  Refactored class name

---
+++ NSKFDC App/nskfdc-app-common/src/main/java/com/nskfdc/scgj/dao/DataImportDao.java
@@ -0,0 +1,116 @@
+package com.nskfdc.scgj.dao;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Repository;
+
+import com.nskfdc.scgj.common.AbstractTransactionalDao;
+import com.nskfdc.scgj.config.DataImportConfig;
+
+@Repository
+public class DataImportDao extends AbstractTransactionalDao{
+	
+	private static final Logger LOGGER= LoggerFactory.getLogger(DataImportDao.class);
+	
+	@Autowired
+	private DataImportConfig dataImportConfig;
+	
+	public void importMasterSheet() {
+		//write Logger here
+		
+				try {
+					
+					//write Logger here 
+					//write code here and change the return type
+				}catch(Exception e) {
+					
+					// write Logger
+					// return null;
+				}
+				
+				
+			}
+	
+	public void getImportHistory() {
+		//write Logger here
+		
+				try {
+					
+					//write Logger here 
+					//write code here and change the return type
+				}catch(Exception e) {
+					
+					// write Logger
+					// return null;
+				}
+				
+				
+			}
+	
+	public void getSearchedMasterSheet() {
+		//write Logger here
+		
+				try {
+					
+					//write Logger here 
+					//write code here and change the return type
+				}catch(Exception e) {
+					
+					// write Logger
+					// return null;
+				}
+				
+				
+			}
+
+	
+	public Integer generateBatchDao(String trainingPartnerEmail){
+					
+			LOGGER.debug("Request received from Service");
+					
+			LOGGER.debug("In  Import Dao, to get create batch for email id:" + trainingPartnerEmail);
+					
+			Map<String, Object> parameters = new HashMap<>();
+					
+			parameters.put("trainingPartnerEmail",trainingPartnerEmail);
+					
+			if(parameters.isEmpty())
+			{
+				LOGGER.error("Null Parameter");
+			}
+			
+			try{
+			        
+				Integer result = getJdbcTemplate().update(dataImportConfig.getGenerateBatch(),parameters);
+						
+				LOGGER.debug("The result of the query is : " + result);
+						
+				return result;
+			}
+					
+			catch(DataAccessException d) {
+						
+				LOGGER.error("DataAccessException in Dao" + d);
+						
+				return -1;
+			}
+					
+			catch(Exception e) {
+						
+				LOGGER.error("In Catch Block");
+						
+				LOGGER.error("An error occured while generating the batch" + e);
+						
+				return -1;
+			}
+	}
+			
+			//write Rowmapper class here
+	
+
+}
