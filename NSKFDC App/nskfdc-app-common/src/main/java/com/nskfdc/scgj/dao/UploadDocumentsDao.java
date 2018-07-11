361db1cfecdc8eb84613b7249be54a7388f06a9e
kamboj02 <40226809+kamboj02@users.noreply.github.com>
2018-07-11 19:57:35 +0530

    author: abhishek kamboj file added: UploadDocumentsDao.java Desciption: Dao File

--- NSKFDC App/nskfdc-app-common/src/main/java/com/nskfdc/scgj/dao/UploadDocumentsDao.java
+++ NSKFDC App/nskfdc-app-common/src/main/java/com/nskfdc/scgj/dao/UploadDocumentsDao.java
@@ -1,17 +1,25 @@
 package com.nskfdc.scgj.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.nskfdc.scgj.common.AbstractTransactionalDao;
+
 import com.nskfdc.scgj.config.UploadDocumentConfig;
+
+import com.nskfdc.scgj.dto.BatchIdDto;
 
 @Repository	
 public class UploadDocumentsDao extends AbstractTransactionalDao{
 
 	@Autowired
 	private UploadDocumentConfig uploadDocumentConfig;
-	
+	private static final BatchIdRowmapper BatchId_RowMapper = new BatchIdRowmapper();
 	public void saveUploadedDocument() {
 		//write Logger here
 		
@@ -49,7 +57,7 @@ public class UploadDocumentsDao extends AbstractTransactionalDao{
 		
 				try {
 					
-					//write Logger here 
+					//write Loggerhere 
 					//write code here and change the return type
 				}catch(Exception e) {
 					
@@ -62,5 +70,35 @@ public class UploadDocumentsDao extends AbstractTransactionalDao{
 
 			
 			//write Rowmapper class here
+	
+	public Collection<BatchIdDto> getBatchDetail(){
+		
+try {
+			
+	System.out.println("in dao");
+			return getJdbcTemplate().query(uploadDocumentConfig.getShowBatchIdDetails(), BatchId_RowMapper);
+			
+		} catch (Exception e) {
+			
+		System.out.println(e);
+			return null;
+			
+		}
+	}
+private static class BatchIdRowmapper implements RowMapper<BatchIdDto>{
+		
+		@Override
+		public BatchIdDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+			
+			String batchId = rs.getString("batchId");
+						
+			
+			return new BatchIdDto(batchId);
+			
+		}
+		
+		
+	}
+
 	
 }
