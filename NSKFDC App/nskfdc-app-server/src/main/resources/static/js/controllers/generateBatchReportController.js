e120f93698929035b13d9288a29d78793b608ceb
SamridhiSmaltnBeryl <40226601+SamridhiSmaltnBeryl@users.noreply.github.com>
2018-07-11 14:25:33 +0530

    author: Samridhi files added:generateBatchReport.html,generateBatchReport.css,generateBatchReportController.js.GenerateBatchReportConfig.class,GenerateBatchReportController.class,GenerateBatchReportDao.java,GenerateBatchReportService.java,GetBatchIdDto.java,getBatchId.yml description: The Batch Report generation files are committed with this.

--- NSKFDC App/nskfdc-app-server/src/main/resources/static/js/controllers/generateBatchReportController.js
+++ NSKFDC App/nskfdc-app-server/src/main/resources/static/js/controllers/generateBatchReportController.js
@@ -2,6 +2,58 @@ var tp = angular.module("app");
 
 tp.controller("generateBatchReportController" , function($scope, $http){
 	
-
+	$scope.details = {
+ 	        enableGridMenus: false,
+ 	        enableSorting: false,
+ 	        enableFiltering: false,
+ 	        enableCellEdit: false,
+ 	        enableColumnMenus: false,
+ 	        enableHorizontalScrollbar: 0,
+ 	        enableVerticalScrollbar: 0,
+ 	        paginationPageSizes: [5, 10, 20, 30],
+ 	        paginationPageSize: 10,
+ 	        useExternalPagination: true,
+ 	        
+ 	       columnDefs: [
+ 	            {
+ 	            	name: 'Id', 
+ 	                displayName: 'Batch Id'
+ 	                	
+ 	            },
+ 	            {
+ 	            	name: 'Date',
+ 	            	displayName: 'Uploaded On'
+ 	            },
+ 	            {
+ 	                name: 'Name',
+ 	                displayName: 'Uploaded By'
+ 	            },
+ 	            {
+ 	                name: 'Download',
+ 	                displayName: 'Download Zip File.'
+ 	            }
+ 	            
+ 	           
+ 	        ]
+ 	    
+     };
+ 	    
+	    
+ 		   var url='/getBatchIdDetails?batchnumber=';
+      	 
+           $scope.ngBlur = function (batchnumber) {
+          	
+          	$scope.ids = [];
+          	
+                  $http.get(url+$scope.batchnumber)
+                  .then(function (response) {
+                	  
+                  	let dt = response.data;	
+                  	for(i in dt){
+                  		$scope.ids.push(response.data[i].batchId); 
+                  	}
+              	});
+          	
+          	 }
 
 });
