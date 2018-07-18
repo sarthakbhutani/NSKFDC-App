d23e112b9f6f3f91472bc55991843e0f287abc85
Sarthak <sarthakbhutani.work@gmail.com>
2018-07-18 16:38:44 +0530

    Author        : Sarthak File Added : uploadDocument.html, uploadDocumentCSS.css, uploadDocumentControllerJS.js Description   : Added necessary frontend files for UploadDocumentsFunctionality

--- NSKFDC App/nskfdc-app-server/src/main/resources/static/js/controllers/uploadDocumentControllerJS.js
+++ NSKFDC App/nskfdc-app-server/src/main/resources/static/js/controllers/uploadDocumentControllerJS.js
@@ -4,18 +4,19 @@ uploadDocument.controller("uploadDocumentController" , function($scope, $http){
 	console.log("working");
 	  $scope.submitForm = function () {
 		  console.log("inside submit form"+$scope.batchID);
-	    alert("Send a request to the server: "+$scope.batchID);
-	    $http.get('/SearchService?batchId='+$scope.batchID)
+	    /*alert("Send a request to the server: "+$scope.batchID);*/
+	    $http.get('/searchDocument?batchId='+$scope.batchID)
 	    .then(function (response) {
 	    	 $scope.details.data= response.data;
 	    	console.log("working after grid");
-
+	    	//downloadZipFileService
 	    });
 	    
 	    
 	  };
 
 	  
+
 	  $scope.details = {
 		        enableGridMenus: false,
 		        enableSorting: false,
@@ -27,14 +28,15 @@ uploadDocument.controller("uploadDocumentController" , function($scope, $http){
 		        paginationPageSizes: [5, 10, 20, 30],
 		        paginationPageSize: 10,
 		        useExternalPagination: true,
-
+//		        rowHeight:70,
+//		        enableColumnResizing: true,
 		        columnDefs: [
 		            {
 		            	name: 'batchId', 
 		                displayName: 'Batch ID'
 		            },
 		            {
-		            	name: 'uploadedOnDate',
+		            	name: 'dateUploaded',
 		            	displayName: 'Uploaded On'
 		            },
 		            {
@@ -42,12 +44,34 @@ uploadDocument.controller("uploadDocumentController" , function($scope, $http){
 		                displayName: 'Documents Uploaded'
 		            },
 		             {
-		            	 name: 'tpDocUrl', 
+		            	 name: 'zipFileLink', 
 		            	 displayName: 'Download Zip File', 
-		            	 cellTemplate: '<a ng-href="{{row.entity.tpDocUrl}}" target="_blank" download><img src="images/rar_icon.png" alt="abc" class="pointer"></a>'
-		             }
+//		            	 cellTemplate: '<a ng-href="file:///D:/sarthak/testZIp/test1.zip" target="_blank" download="test1.zip"><img src="images/rar_icon.png" alt="abc" class="pointer"></a>'
+		            	 cellTemplate: '<img src="images/rar_icon.png" alt="abc" ng-click="grid.appScope.downloadZip()" class="pointer">'
+				             
+		            }
 		        ]
+//	  ,enableColumnResizing: true
 		    };
+	  	  
+	  //zip download start
+	  $scope.downloadZip = function(){
+		  var url='/downloadZipFileService?batchId='+$scope.batchID;
+		  console.log("This is downloading")
+		  $http.get(url, { responseType : 'arraybuffer' })
+		  .then(function(response){
+			  console.log("done"+$scope.batchID + response.data);
+			  var zipFile = new Blob([response.data], { type : 'application/zip' })
+			  var downloadURL = URL.createObjectURL(zipFile);
+			  var link = document.createElement('a');
+			  //from here:: move the rest from up
+			  link.href = downloadURL;
+			  link.download = $scope.batchID + '.zip';
+			  document.body.appendChild(link);
+			  link.click();
+		  });
+	  };
+	  //zip download end
     
 //  			$scope.getBatchId = function()
 //  			{
@@ -75,5 +99,58 @@ uploadDocument.controller("uploadDocumentController" , function($scope, $http){
 		    });
 
 	  
-	});
+	  $scope.getTableHeight = function(){
+			var rowHeight=30;
+			var headerHeight =30;
+			return{
+				height:($scope.details.data.length* rowHeight + headerHeight) +"px"
+			};
+		  };
+		  
+		  var content = 'file content for example';
+		  var blob = new Blob([ content ], { type : 'text/plain' });
+		  $scope.url = (window.URL || window.webkitURL).createObjectURL( blob );
+		  
+		  
+		  $scope.submitForm1 = function (){
+				 console.log("working3");
+				
+			
+			console.log($scope.myVar2);
+			console.log($scope.myVar3 );
+			$http.get("/getScgjDetails?batchId="+$scope.myVar2+"&scgjBatchNumber="+$scope.myVar3)
+			.then(function (response){
+				console.log("working5");
+				$scope.status= response.data;
+				if($scope.status==1 || $scope.scgjId!=null){
+					document.getElementById("error_msg1").innerHTML="";	
+					document.getElementById("error_msg2").innerHTML="";	
+					alert("upload successfull!!");
+					
+				}
+				else{
+					console.log("working10");
+					 document.getElementById("error_msg1").innerHTML="Invalid BatchId";
+					 document.getElementById("error_msg2").innerHTML="Invalid ScgjID";
+					 
+					
+		               
+				}
+			
+			});
+			 };
+			 $scope.enable = function() {
+			        console.log("inside function " + $scope.value);
+			        $scope.isDisabled = false;
+			 };
+			 
+			 
+			
+				  
+
+
+});
+
+
+//	});
 	
