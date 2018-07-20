4fd3fe0e1db0b911d283095b899f6058cbc71437
poojasharma06 <sharmapooja6797@gmail.com>
2018-07-20 13:35:13 +0530

    Author:           Pooja Sharma Files Modified:  SCGJ_admin.html, trainingPartner.html, SCGJ_admin.css, trainingPartner.css, SCGJ_adminControlllerJS.js, trainingPartnerControllerJS.js

--- NSKFDC App/nskfdc-app-server/src/main/resources/static/js/controllers/SCGJ_adminControlllerJS.js
+++ NSKFDC App/nskfdc-app-server/src/main/resources/static/js/controllers/SCGJ_adminControlllerJS.js
@@ -3,5 +3,9 @@ var scgj = angular.module("app");
 scgj.controller("SCGJUserController" , function($scope, $http,$rootScope){
 	
 	$rootScope.mainTemplate=false;
+	$scope.alpha = true;
+	$scope.beta = false;
+	$scope.gamma = false;
+	
 
 });
--- NSKFDC App/nskfdc-app-server/src/main/resources/static/js/controllers/trainingPartnerControllerJS.js
+++ NSKFDC App/nskfdc-app-server/src/main/resources/static/js/controllers/trainingPartnerControllerJS.js
@@ -3,8 +3,10 @@ var trainingPartner = angular.module("app");
 trainingPartner.controller("trainingPartnerController" , function($scope, $http,$rootScope){
 	
 	$rootScope.mainTemplate=false;
-	
-	//write your code here
+	$scope.alpha = true;
+	$scope.beta = false;
+	$scope.gamma = false;
+	$scope.delta = false;
 
 	
 });
