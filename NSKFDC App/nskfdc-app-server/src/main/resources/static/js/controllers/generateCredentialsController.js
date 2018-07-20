7543c248444a93ab5914c313bae245bb8ddc2c85
Priyankachaudhary2606 <priyanka@smaltandberyl.com>
2018-07-20 08:54:31 +0530

    Author   : Priyanka Chaudhary Description : frontend fixed for generateCredentials Files Updated : generateCredentials.css, generateCredentials.html, generateCredentialsController.js

--- NSKFDC App/nskfdc-app-server/src/main/resources/static/js/controllers/generateCredentialsController.js
+++ NSKFDC App/nskfdc-app-server/src/main/resources/static/js/controllers/generateCredentialsController.js
@@ -1,6 +1,23 @@
 var scgj = angular.module("app");
 
 scgj.controller("generateCredentialsController" , function($scope, $http){
+	
+	$scope.credentialTable= false;
+	
+	
+	/*---------------------------- EYE TOGGLE --------------------------------*/
+	$scope.inputTypePassword="password";
+	$scope.eyeToggleGenerateCredential=function(){
+		if($scope.inputTypePassword=="password"){
+			$scope.inputTypePassword="text";
+		}
+		else{
+			$scope.inputTypePassword="password";
+		}
+	}
+	
+	
+	
 	$scope.credentialErrorMessage=false;
 	 $scope.detailsData = {
 		        enableGridMenus: false,
@@ -8,8 +25,7 @@ scgj.controller("generateCredentialsController" , function($scope, $http){
 		        enableFiltering: false,
 		        enableCellEdit: false,
 		        enableColumnMenus: false,
-		        enableHorizontalScrollbar: 0,
-		        enableVerticalScrollbar: 0,
+		   
 		        paginationPageSizes: [5, 10, 20, 30],
 		        paginationPageSize: 10,
 		        useExternalPagination: true,
@@ -63,6 +79,7 @@ scgj.controller("generateCredentialsController" , function($scope, $http){
 				{
 				$scope.credentialErrorMessage=false;
 				$scope.credentialSuccessMessage=true;
+				$scope.data=null;
 				}
 		 });
 
@@ -76,29 +93,37 @@ scgj.controller("generateCredentialsController" , function($scope, $http){
 	 });
 		   
 		    
-	var pwd = document.getElementById('password')
-	var eye = document.getElementById('eye')
-
-
-	 eye.addEventListener('click',togglePass);
-
-	 function togglePass()
-	 {
-        eye.classList.toggle('active');	    
-	     pwd.type == ('password') ? pwd.type = 'text' :
-	        pwd.type = 'password';
-	 }
-	 
-//	 $scope.emailadd = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
 
 	 $scope.search=function(nsdcRegNumber) {
 		 console.log($scope.nsdcRegNumber);
 		 $http.get("/SearchService?nsdcRegNumber="+$scope.nsdcRegNumber)
 			.then(function (response) {	
-				 console.log(response.data);
-		 $scope.detailsData.data=response.data;
+				 
+				 if(response.data.length==0){
+					 $scope.searchError='No Data Found';
+				 }
+				 else{
+				 $scope.detailsData.data=response.data;
+				 $scope.searchError='';
+				 }
 			});
 	 }
 	 
+	
+	 
+	 /*----------- Grid Height -------------*/
+	 $scope.getTableHeight=function(){
+		 var rowHeight=30;
+		 var headerHeight=30;
+		 if($scope.detailsData.data.length==0){
+			 $scope.credentialTable= false;
+		 }
+		 else{
+			 $scope.credentialTable= true;
+		 return{
+			 height:($scope.detailsData.data.length * rowHeight + headerHeight)+"px"
+		 };
+	 }
+	 };
 
 });
