e2a414fe655a0e89746df39f8c5a1cdb90bd74de
ruchipareek5 <ruchipareek5@gmail.com>
2018-07-31 16:25:20 +0530

    Author                             :Ruchi Pareek Description                        :removed traget folder from the git repository and added to ignore list

--- NSKFDC App/nskfdc-app-common/src/main/java/com/nskfdc/scgj/common/WebSecurityConfig.java
+++ NSKFDC App/nskfdc-app-common/src/main/java/com/nskfdc/scgj/common/WebSecurityConfig.java
@@ -1,14 +1,5 @@
 package com.nskfdc.scgj.common;
 
-
-import java.io.IOException;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
@@ -18,12 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.security.web.csrf.CsrfToken;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
-import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.util.WebUtils;
 
 
 
