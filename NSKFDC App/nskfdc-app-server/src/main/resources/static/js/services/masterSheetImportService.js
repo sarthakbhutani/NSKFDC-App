0118eff7dfd8c7417a8a5f986d2397dccd40ddf4
Priyankachaudhary2606 <priyanka@smaltandberyl.com>
2018-08-04 14:31:41 +0530

    Author         : Priyanka Chaudhary Description    : Fixed the bug of error messages for Excel sheet Import Files Updated  : masterSheetImport.html, masterSheetImportService.js, masterSheetImport.css

--- NSKFDC App/nskfdc-app-server/src/main/resources/static/js/services/masterSheetImportService.js
+++ NSKFDC App/nskfdc-app-server/src/main/resources/static/js/services/masterSheetImportService.js
@@ -1,19 +1,39 @@
 app.service('fileUpload', ['$http', function ($http) {
 	this.uploadFileToUrl = function (file, importUrl, batchId ) {
 		var fd = new FormData();
-//        console.log('File is :'+file);
+
         fd.append('file', file);
         fd.append('batchId', batchId);
-//        console.log('batchId:---'+batchId);
-//        console.log('FILE-----'+file);
+
         
         $http({
         	method: 'POST',
         	url: importUrl,
             data: fd,
             headers: {'Content-Type': undefined},
+            transformRequest: angular.identity,
+            transformResponse: [function (data) {
+            	thisIsResponse=data;
+            	return data;
+          
+            }]
             
         }).then(function(response) {
+        	var errorMessage = thisIsResponse;
+			if(thisIsResponse=="File Uploaded Successfully")
+				{
+				document.getElementById("importExcelMessage").innerHTML=errorMessage;
+				document.getElementById("importExcelMessage").style.color = "green";
+				
+				}
+			else{
+	        	document.getElementById("importExcelMessage").innerHTML=errorMessage;
+	        	document.getElementById("importExcelMessage").style.color = "red";
+			}
+            return response.data;
+           
+        
+        	
 	});
 
 }
