7eeee4c06cdb80553f2c82d7e48fa943ab70b686
prateek-kapoor323 <prateek@smaltandberyl.com>
2018-08-24 23:23:59 +0530

    Author                : Prateek Kapoor Files Modified        : createTableMaster.sql Description           : Changed data type of batch ID to varchar

--- DbScript/createTableMaster.sql
+++ DbScript/createTableMaster.sql
@@ -1,42 +1,50 @@
-drop database if exists nskfdc;
-create database nskfdc;
-use nskfdc;
+CREATE DATABASE  IF NOT EXISTS `nskfdc` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `nskfdc`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: nskfdc
+-- ------------------------------------------------------
+-- Server version	5.7.18-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bankdetails`
+--
 
 DROP TABLE IF EXISTS `bankdetails`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `bankdetails` (
+  `accountNumber` bigint(20) NOT NULL,
+  `ifscCode` varchar(50) DEFAULT NULL,
+  `bankName` varchar(50) DEFAULT NULL,
+  `enrollmentNumber` varchar(50) NOT NULL,
+  PRIMARY KEY (`accountNumber`),
+  KEY `enrollmentNumber_idx` (`enrollmentNumber`),
+  CONSTRAINT `enrollmentNumber` FOREIGN KEY (`enrollmentNumber`) REFERENCES `candidate` (`enrollmentNumber`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `batchdetails`
+--
+
 DROP TABLE IF EXISTS `batchdetails`;
-
-DROP TABLE IF EXISTS `candidate`;
-DROP TABLE IF EXISTS `centredetails`;
-DROP TABLE IF EXISTS `employerdetails`;
-DROP TABLE IF EXISTS `generatereports`;
-DROP TABLE IF EXISTS `trainingpartnerdetails`;
-DROP TABLE IF EXISTS `uploadeddocuments`;
-DROP TABLE IF EXISTS `user`;
-
-
-CREATE TABLE `user` (
-  `userId` int(11) NOT NULL AUTO_INCREMENT,
-  `userEmail` varchar(100) DEFAULT NULL,
-  `password` varchar(250) DEFAULT NULL,
-  `role` varchar(45) DEFAULT NULL,
-  `generatedOn` date DEFAULT NULL,
-  PRIMARY KEY (`userId`),
-  UNIQUE KEY `trainingPartnerEmail_UNIQUE` (`userEmail`)
-);
-
-CREATE TABLE `centredetails` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `centreId` bigint(20) DEFAULT NULL,
-  `centreState` varchar(60) DEFAULT NULL,
-  `centreCity` varchar(60) DEFAULT NULL,
-  `userEmail` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `centreId_UNIQUE` (`centreId`),
-  KEY `traningPartnerEmail_idx` (`userEmail`),
-  CONSTRAINT `traningPartnerEmail` FOREIGN KEY (`userEmail`) REFERENCES `user` (`userEmail`) ON DELETE NO ACTION ON UPDATE NO ACTION
-);
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `batchdetails` (
-  `batchId` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `batchId` varchar(200) DEFAULT NULL,
   `batchStartDate` date DEFAULT NULL,
   `batchEndDate` date DEFAULT NULL,
   `assessmentDate` date DEFAULT NULL,
@@ -50,14 +58,22 @@ CREATE TABLE `batchdetails` (
   `userEmail` varchar(100) NOT NULL,
   `centreId` bigint(20) DEFAULT NULL,
   `updatedOn` date DEFAULT NULL,
-  PRIMARY KEY (`batchId`),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `batchId_UNIQUE` (`batchId`),
   KEY `tpEmail_idx` (`userEmail`),
   KEY `centreId_idx` (`centreId`),
   CONSTRAINT `centreId` FOREIGN KEY (`centreId`) REFERENCES `centredetails` (`centreId`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `tpEmail` FOREIGN KEY (`userEmail`) REFERENCES `user` (`userEmail`) ON DELETE NO ACTION ON UPDATE NO ACTION
-);
+) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `candidate`
+--
 
+DROP TABLE IF EXISTS `candidate`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `candidate` (
   `enrollmentNumber` varchar(50) NOT NULL,
   `salutation` varchar(50) DEFAULT NULL,
@@ -85,44 +101,81 @@ CREATE TABLE `candidate` (
   `workplaceAddress` varchar(100) DEFAULT NULL,
   `assessmentResult` varchar(45) DEFAULT NULL,
   `medicalExamConducted` varchar(50) DEFAULT NULL,
-  `batchId` int(11) NOT NULL AUTO_INCREMENT,
   `relationWithSKMS` varchar(45) DEFAULT NULL,
+  `batchId` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`enrollmentNumber`),
+  UNIQUE KEY `batchId_UNIQUE` (`batchId`),
   KEY `batchId_idx` (`batchId`),
   CONSTRAINT `batchId` FOREIGN KEY (`batchId`) REFERENCES `batchdetails` (`batchId`) ON DELETE NO ACTION ON UPDATE NO ACTION
-);
-CREATE TABLE `bankdetails` (
-  `accountNumber` bigint(20) NOT NULL,
-  `ifscCode` varchar(50) DEFAULT NULL,
-  `bankName` varchar(50) DEFAULT NULL,
-  `enrollmentNumber` varchar(50) NOT NULL,
-  PRIMARY KEY (`accountNumber`),
-  KEY `enrollmentNumber_idx` (`enrollmentNumber`),
-  CONSTRAINT `enrollmentNumber` FOREIGN KEY (`enrollmentNumber`) REFERENCES `candidate` (`enrollmentNumber`) ON DELETE NO ACTION ON UPDATE NO ACTION
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE employerdetails (
-  employerId int(11) primary key auto_increment,
-  employerName varchar(60) DEFAULT NULL,
-  employerContactNumber bigint(20) DEFAULT NULL,
-  batch_id int(11) NOT NULL,
-  user_email varchar(100) NOT NULL,
-  foreign key (batch_id) references batchdetails(batchId),
-  foreign key (user_email) references user(userEmail)
-);
+--
+-- Table structure for table `centredetails`
+--
 
+DROP TABLE IF EXISTS `centredetails`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `centredetails` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `centreId` bigint(20) DEFAULT NULL,
+  `centreState` varchar(60) DEFAULT NULL,
+  `centreCity` varchar(60) DEFAULT NULL,
+  `userEmail` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `centreId_UNIQUE` (`centreId`),
+  KEY `traningPartnerEmail_idx` (`userEmail`),
+  CONSTRAINT `traningPartnerEmail` FOREIGN KEY (`userEmail`) REFERENCES `user` (`userEmail`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `employerdetails`
+--
+
+DROP TABLE IF EXISTS `employerdetails`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `employerdetails` (
+  `employerId` int(11) NOT NULL AUTO_INCREMENT,
+  `employerName` varchar(60) DEFAULT NULL,
+  `employerContactNumber` bigint(20) DEFAULT NULL,
+  `user_email` varchar(200) DEFAULT NULL,
+  `batchId` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`employerId`),
+  UNIQUE KEY `batchId_UNIQUE` (`batchId`),
+  KEY `user_email` (`user_email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `generatereports`
+--
+
+DROP TABLE IF EXISTS `generatereports`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `generatereports` (
   `generateReportId` varchar(100) NOT NULL,
   `reportType` varchar(40) DEFAULT NULL,
   `generatedOn` datetime DEFAULT NULL,
   `userEmail` varchar(100) NOT NULL,
-  `batchId` int(11) DEFAULT NULL,
+  `batchId` varchar(100) NOT NULL,
   PRIMARY KEY (`generateReportId`),
   KEY `tpreportmail_idx` (`userEmail`),
-  KEY `batchId_idx` (`batchId`),
-  CONSTRAINT `batchIdFKey` FOREIGN KEY (`batchId`) REFERENCES `batchdetails` (`batchId`) ON DELETE NO ACTION ON UPDATE NO ACTION
-);
+  KEY `batchId` (`batchId`),
+  CONSTRAINT `generatereports_ibfk_1` FOREIGN KEY (`batchId`) REFERENCES `batchdetails` (`batchId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `trainingpartnerdetails`
+--
+
+DROP TABLE IF EXISTS `trainingpartnerdetails`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `trainingpartnerdetails` (
   `nsdcRegNumber` varchar(50) NOT NULL,
   `trainingPartnerName` varchar(50) DEFAULT NULL,
@@ -134,7 +187,16 @@ CREATE TABLE `trainingpartnerdetails` (
   PRIMARY KEY (`nsdcRegNumber`),
   KEY `trainingPartnerEmail_idx` (`userEmail`),
   CONSTRAINT `trainingPartnerEmail` FOREIGN KEY (`userEmail`) REFERENCES `user` (`userEmail`) ON DELETE NO ACTION ON UPDATE NO ACTION
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `uploadeddocuments`
+--
+
+DROP TABLE IF EXISTS `uploadeddocuments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `uploadeddocuments` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `finalBatchReport` tinyint(1) DEFAULT NULL,
@@ -151,10 +213,40 @@ CREATE TABLE `uploadeddocuments` (
   `attendanceSheetPath` varchar(500) DEFAULT NULL,
   `dateUploaded` date DEFAULT NULL,
   `userEmail` varchar(100) NOT NULL,
-  `batchId` int(11) NOT NULL,
+  `batchId` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `tpmail_idx` (`userEmail`),
-  KEY `fk_1` (`batchId`),
-  CONSTRAINT `fk_1` FOREIGN KEY (`batchId`) REFERENCES `batchdetails` (`batchId`),
-  CONSTRAINT `tpmail` FOREIGN KEY (`userEmail`) REFERENCES `user` (`userEmail`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) 
+  KEY `batchId` (`batchId`),
+  CONSTRAINT `tpmail` FOREIGN KEY (`userEmail`) REFERENCES `user` (`userEmail`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `uploadeddocuments_ibfk_1` FOREIGN KEY (`batchId`) REFERENCES `batchdetails` (`batchId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `userId` int(11) NOT NULL AUTO_INCREMENT,
+  `userEmail` varchar(100) DEFAULT NULL,
+  `password` varchar(250) DEFAULT NULL,
+  `role` varchar(45) DEFAULT NULL,
+  `generatedOn` date DEFAULT NULL,
+  PRIMARY KEY (`userId`),
+  UNIQUE KEY `trainingPartnerEmail_UNIQUE` (`userEmail`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-08-24 23:20:02
