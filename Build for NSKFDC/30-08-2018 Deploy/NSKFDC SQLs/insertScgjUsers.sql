c4a3b2a5399c799902be37d67a9126775cba2d6e
prateek-kapoor323 <prateek@smaltandberyl.com>
2018-10-08 17:24:12 +0530

    Author         : Prateek Kapoor File Modified  : Build For NSKFDC Description    : This folder contains Build for NSKFDC

---
+++ Build for NSKFDC/30-08-2018 Deploy/NSKFDC SQLs/insertScgjUsers.sql
@@ -0,0 +1,4 @@
+use nskfdc;
+INSERT INTO `user` VALUES (1,'ceo@sscgj.in','ceo1955','scgj',NULL);
+INSERT INTO `user` VALUES (2,'kamal@sscgj.in','kamal123#','scgj',NULL);
+INSERT INTO `user` VALUES (3,'prashant@sscgj.in','prashant123#','scgj',NULL);
