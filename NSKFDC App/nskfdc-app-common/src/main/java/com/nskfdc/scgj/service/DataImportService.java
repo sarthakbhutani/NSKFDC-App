f2b7bf280ecdee9b21b0d9a1e766a867e2d4c8d7
Priyankachaudhary2606 <priyanka@smaltandberyl.com>
2018-10-12 12:04:37 +0530

    Author         : Priyanka Chaudhary Description    : Updated the build number to 778 in release notes for deployment on 12-10-2018, 12:00PM, Removed JUnit import in DataImportService.java Files Updated  : DataImportService.java, 12-10-2018-release.txt

--- NSKFDC App/nskfdc-app-common/src/main/java/com/nskfdc/scgj/service/DataImportService.java
+++ NSKFDC App/nskfdc-app-common/src/main/java/com/nskfdc/scgj/service/DataImportService.java
@@ -25,7 +25,6 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
